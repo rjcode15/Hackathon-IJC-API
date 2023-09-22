721165d12b7d060a21a2039b80c2245b9dc84a02
Ajay Nara <ajay.nara2696@gmail.com>
2023-09-22 14:28:20 -0400

    Java Version Change

--- src/main/java/com/pa/vote/controller/ElectionController.java
+++ src/main/java/com/pa/vote/controller/ElectionController.java
@@ -1,6 +1,7 @@
 package com.pa.vote.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pa.vote.service.GoogleCivicService;
 
 @RestController
+@CrossOrigin
 public class ElectionController {
 	
 	@Autowired
@@ -19,7 +21,7 @@ public class ElectionController {
 		return civicService.getElectionInfo();
 	}
 	
-	@GetMapping(value = "/v1/elections/voting-info")
+	@GetMapping(value = "/v1/elections/polling-location-info")
 	public String getVotingInfo(@RequestParam String address, @RequestParam String electionId) {
 		
 		return civicService.getVotingInfo(address,electionId);
